e7523b5359aa93ce80739924775737bfcb857424
Chris Thomas <chris.c.thomas@icloud.com>
2021-06-07 11:17:05 -0500

    Update configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,23 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      cyan: '#0084ac',
+      orange: '#ff6900',
+      green: '#7cb82f',
+      black: '#333333',
+      darkGray: '#888888',
+      lightGray: '#b2b2b2',
+    },
+    fontFamily: {
+      'brandon-grotesque': ['Brandon Grotesque'],
+      'open-sans': ['Open Sans'],
+    },
+    fontSize: {
+      'sm': '0.875rem',
+      'base': '1rem',
+      '6xl': '4rem',
+    },
   },
   variants: {
     extend: {},
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 4000
+  },
   plugins: [
     vue(),
     legacy({
