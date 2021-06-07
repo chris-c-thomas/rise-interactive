c1720fa887160153b7db6e1a31b2ed18f351d155
Chris Thomas <chris.c.thomas@icloud.com>
2021-06-07 11:14:52 -0500

    Add initial navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="mx-auto">
+  <nav class="flex flex-row justify-around h-16 bg-white border-b-2 border-black border-opacity-25 shadow-lg">
+    <img src="../assets/CheeseWorld.png" alt="Cheese World Logo" class="self-center w-36">
+    <a href="#" class="self-center text-lg text-black font-open-sans font-bold">Health &amp; Wellness</a>
+    <a href="#" class="self-center text-lg text-black font-open-sans font-normal">Sustainability</a>
+    <a href="#" class="self-center text-lg text-black font-open-sans font-normal">Facts &amp; Myths</a>
+    <a href="#" class="self-center text-lg text-black font-open-sans font-normal">Producers</a>
+    <a href="#" class="self-center text-lg text-black font-open-sans font-normal">Recipes</a>
+    <input type="text" class="self-center border-darkGray border-2 rounded-xl w-52 h-8">
+  </nav>
+</div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
+
