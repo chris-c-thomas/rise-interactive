c20a19ef80d095fdbce1f39df801914136d7260c
Chris Thomas <chris.c.thomas@icloud.com>
2021-06-07 11:33:13 -0500

    Change logo path

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="mx-auto">
   <nav class="flex flex-row justify-around h-16 bg-white border-b-2 border-black border-opacity-25 shadow-lg">
-    <img src="../assets/CheeseWorld.png" alt="Cheese World Logo" class="self-center w-36">
+    <img src="../assets/img/CheeseWorld.png" alt="Cheese World Logo" class="self-center w-36">
     <a href="#" class="self-center text-lg text-black font-open-sans font-bold">Health &amp; Wellness</a>
     <a href="#" class="self-center text-lg text-black font-open-sans font-normal">Sustainability</a>
     <a href="#" class="self-center text-lg text-black font-open-sans font-normal">Facts &amp; Myths</a>
