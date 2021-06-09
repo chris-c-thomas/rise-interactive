7595683eb3b8179527c12cfa0c57b70da87a8235
Chris Thomas <chris.c.thomas@icloud.com>
2021-06-09 16:41:45 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
       orange: '#ff6900',
       green: '#7cb82f',
       black: '#333333',
+      white: '#ffffff',
       darkGray: '#888888',
       lightGray: '#b2b2b2',
     },
@@ -18,8 +19,14 @@ module.exports = {
     fontSize: {
       'sm': '0.875rem',
       'base': '1rem',
+      'lg': '1.125rem',
+      'xl': '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
       '6xl': '4rem',
-    },
+    }
   },
   variants: {
     extend: {},
