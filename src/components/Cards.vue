eb7d39c5be06f4d5b1a0d0f96a2218f9dab60d17
Chris Thomas <chris.c.thomas@icloud.com>
2021-06-22 09:08:00 -0500

    See More button hover effect

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -106,11 +106,21 @@
     display: none;
   }
 
+  .see-more a:hover,
+  .see-more a:focus {
+    background-color: #ffffff;
+    color: #ff6900;
+  }
+
   @media screen and (min-width: 1024px) {
     .cards {
       display: grid;
       grid-template-columns: 1fr 1fr 1fr 1fr;
       grid-template-rows: 1fr 1fr 1fr;
+      grid-template-areas: 
+        "card-1 card-2 card-3"
+        "card-4 card-5 card-6 card-7"
+        "card-8 card-9 card-10 card-11";
       column-gap: 20px;
     }
     .card {
